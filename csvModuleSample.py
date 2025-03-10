b829af554a4e24a6be6c68d86be20b43cb138342
WatoLua <private@email.com>
2025-03-10 16:29:50 +0100

    added to serial for matrix profile

--- csvModuleSample.py
+++ csvModuleSample.py
@@ -1,7 +1,10 @@
 #this module is an example that copy the csv in new csv file using threads
 import threading
 
+import csvUtils
 import fileUtils
+
+##your module to import, or it can be a class
 
 localVars = {}
 
@@ -18,3 +21,19 @@ def endProcess():
 def process(row):
     line = localVars["separator"].join(row)
     fileUtils.addToFile(localVars["outputFile"], line)
+
+##end your module
+
+def main():
+
+    module.init()
+
+    csvReader = csvUtils.CsvReader()
+    csvProcessor = csvUtils.CsvProcessor(csvReader, module, 1, "each")
+    csvReader.readCsv("your_csv", ";", None)
+    csvProcessor.processCsv(1)
+
+    module.endProcess()
+
+
+main()
