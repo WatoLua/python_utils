aa4f9c6ef1ef0ae162258ed70296d62e26bcdba5
WatoLua <private@email.com>
2025-07-31 11:38:23 +0200

    added proxy when fetching url

--- urlUtils.py
+++ urlUtils.py
@@ -2,9 +2,16 @@ import json
 from urllib.parse import urlparse
 import requests
 
+localVars = {
+    "url_proxy": None
+}
+
 def ignoreWarnings():
     import urllib3
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def withProxy(proxy):
+    localVars["url_proxy"] = proxy
 
 def extract_host(url: str) -> str:
     parsed_url = urlparse(url)
@@ -22,10 +29,10 @@ def fetchWithJson(method, url, body=None, printErrors = True):
         raise ValueError("Unhandled or invalid HTTP method specified")
 
     if body == None:
-        response = requestFunction[method.upper()](url, verify=False)
+        response = requestFunction[method.upper()](url, verify=False, proxies=localVars["url_proxy"])
     else:
         response = requestFunction[method.upper()](url, json=body,
-            headers={"Content-Type": "application/json"}, verify=False)
+            headers={"Content-Type": "application/json"}, verify=False, proxies=localVars["url_proxy"])
 
     if response.status_code != 200:
         if printErrors:
