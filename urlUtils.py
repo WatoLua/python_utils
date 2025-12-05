23a08b36d02a233718fa85ae81f7223189f866a5
WatoLua <private@email.com>
2025-12-05 15:27:56 +0100

    add fetchWithJsonV2 to handle HTTP requests with improved error handling and method validation

--- urlUtils.py
+++ urlUtils.py
@@ -17,6 +17,31 @@ def extract_host(url: str) -> str:
     parsed_url = urlparse(url)
     return parsed_url.netloc  # Renvoie le host (nom de domaine ou IP avec port)
 
+def fetchWithJsonV2(method, url, header=None, body=None, printErrors = True):
+    requestFunction = {
+        "GET": requests.get,
+        "POST": requests.post,
+        "PUT": requests.put,
+        "DELETE": requests.delete
+    }
+
+    if method.upper() not in requestFunction:
+        raise ValueError("Unhandled or invalid HTTP method specified")
+
+    if body == None:
+        response = requestFunction[method.upper()](url, headers=header, verify=False, proxies=localVars["url_proxy"])
+    else:
+        response = requestFunction[method.upper()](url, json=body,
+                                                   headers=header, verify=False,
+                                                   proxies=localVars["url_proxy"])
+
+    if response.status_code != 200:
+        if printErrors:
+            print(f"Got error when requested {url} : {response.status_code} : {response.text}")
+        return None
+    else:
+        return json.loads(response.text)
+
 def fetchWithJson(method, url, body=None, printErrors = True):
     requestFunction = {
         "GET": requests.get,
